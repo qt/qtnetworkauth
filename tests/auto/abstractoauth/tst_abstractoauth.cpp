86c5036848ad004208803d9d497cfcd08c9623d6
Jani Heikkinen <jani.heikkinen@qt.io>
2017-05-03 08:59:09 +0300

    Fix licensing
    - Remove unused LICENSE.GPLv3 license file & add missing
    LICENSE.GPL3-EXCEPT
    - Examples are licensed under BSD nowdays, update correct
    license headers there
    - Test are licensed under GPL-EXCEPT nowdays, update
    correct license header there
    
    Change-Id: If826712993e14f434bcca74821348880563d61ae
    Reviewed-by: Lars Knoll <lars.knoll@qt.io>
    Reviewed-by: Jesus Fernandez <Jesus.Fernandez@qt.io>

--- tests/auto/abstractoauth/tst_abstractoauth.cpp
+++ tests/auto/abstractoauth/tst_abstractoauth.cpp
@@ -1,11 +1,11 @@
 /****************************************************************************
 **
-** Copyright (C) 2016 The Qt Company Ltd.
+** Copyright (C) 2017 The Qt Company Ltd.
 ** Contact: https://www.qt.io/licensing/
 **
 ** This file is part of the Qt Network Auth module of the Qt Toolkit.
 **
-** $QT_BEGIN_LICENSE:GPL$
+** $QT_BEGIN_LICENSE:GPL-EXCEPT$
 ** Commercial License Usage
 ** Licensees holding valid commercial Qt licenses may use this file in
 ** accordance with the commercial license agreement provided with the
@@ -16,9 +16,8 @@
 **
 ** GNU General Public License Usage
 ** Alternatively, this file may be used under the terms of the GNU
-** General Public License version 3 or (at your option) any later version
-** approved by the KDE Free Qt Foundation. The licenses are as published by
-** the Free Software Foundation and appearing in the file LICENSE.GPL3
+** General Public License version 3 as published by the Free Software
+** Foundation with exceptions as appearing in the file LICENSE.GPL3-EXCEPT
 ** included in the packaging of this file. Please review the following
 ** information to ensure the GNU General Public License requirements will
 ** be met: https://www.gnu.org/licenses/gpl-3.0.html.
