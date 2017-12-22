qt/qtnetworkauth

commit c071b588ce2b4d474845d553aa83120747dc46da

Move some functions to base classes to avoid virtual
OAuth base classes now support responseType, in OAuth2, the basic
client identifier and the basic token (without secrets). The base
class methods can be called to get and set the properties without
needing to override pure virtual member functions.

Task-number: QTBUG-60809
Change-Id: Id6b2aebe1a07e62c937078b5206aade53a9a85eb
Reviewed-by: Edward Welbourne <edward.welbourne@qt.io>

src/oauth/qoauth1.cpp
@@ -64,8 +64,8 @@ const QString Key::oauthVersion =            QStringLiteral("oauth_version");
 
 QOAuth1Private::QOAuth1Private(const QPair<QString, QString> &clientCredentials,
                                QNetworkAccessManager *networkAccessManager) :
-    QAbstractOAuthPrivate(networkAccessManager),
-    clientCredentials(clientCredentials)
+    QAbstractOAuthPrivate(QUrl(), clientCredentials.first, networkAccessManager),
+    clientIdentifierSharedKey(clientCredentials.second)
 {
     qRegisterMetaType<QNetworkReply::NetworkError>("QNetworkReply::NetworkError");
     qRegisterMetaType<QOAuth1::SignatureMethod>("QOAuth1::SignatureMethod");
@@ -76,7 +76,7 @@ void QOAuth1Private::appendCommonHeaders(QVariantMap *headers)
     const auto currentDateTime = QDateTime::currentDateTimeUtc();
 
     headers->insert(Key::oauthNonce, QOAuth1::nonce());
-    headers->insert(Key::oauthConsumerKey, clientCredentials.first);
+    headers->insert(Key::oauthConsumerKey, clientIdentifier);
     headers->insert(Key::oauthTimestamp, QString::number(currentDateTime.toTime_t()));
     headers->insert(Key::oauthVersion, oauthVersion);
     headers->insert(Key::oauthSignatureMethod, signatureMethodString().toUtf8());
@@ -183,8 +183,8 @@ QByteArray QOAuth1Private::generateSignature(const QVariantMap &parameters,
                                              QNetworkAccessManager::Operation operation) const
 {
     const QOAuth1Signature signature(url,
-                                     clientCredentials.second,
-                                     tokenCredentials.second,
+                                     clientIdentifierSharedKey,
+                                     tokenSecret,
                                      static_cast<QOAuth1Signature::HttpRequestMethod>(operation),
                                      parameters);
 
@@ -192,7 +192,7 @@ QByteArray QOAuth1Private::generateSignature(const QVariantMap &parameters,
     case QOAuth1::SignatureMethod::Hmac_Sha1:
         return signature.hmacSha1().toBase64();
     case QOAuth1::SignatureMethod::PlainText:
-        return signature.plainText(clientCredentials.first);
+        return signature.plainText(clientIdentifier);
     default:
         qFatal("QOAuth1Private::generateSignature: Signature method not supported");
         return QByteArray();
@@ -255,32 +255,17 @@ QOAuth1::QOAuth1(const QString &clientIdentifier,
 QOAuth1::~QOAuth1()
 {}
 
-QString QOAuth1::clientIdentifier() const
-{
-    Q_D(const QOAuth1);
-    return d->clientCredentials.first;
-}
-
-void QOAuth1::setClientIdentifier(const QString &clientIdentifier)
-{
-    Q_D(QOAuth1);
-    if (d->clientCredentials.first != clientIdentifier) {
-        d->clientCredentials.first = clientIdentifier;
-        Q_EMIT clientIdentifierChanged(clientIdentifier);
-    }
-}
-
 QString QOAuth1::clientSharedSecret() const
 {
     Q_D(const QOAuth1);
-    return d->clientCredentials.second;
+    return d->clientIdentifierSharedKey;
 }
 
 void QOAuth1::setClientSharedSecret(const QString &clientSharedSecret)
 {
     Q_D(QOAuth1);
-    if (d->clientCredentials.second != clientSharedSecret) {
-        d->clientCredentials.second = clientSharedSecret;
+    if (d->clientIdentifierSharedKey != clientSharedSecret) {
+        d->clientIdentifierSharedKey = clientSharedSecret;
         Q_EMIT clientSharedSecretChanged(clientSharedSecret);
     }
 }
@@ -288,7 +273,7 @@ void QOAuth1::setClientSharedSecret(const QString &clientSharedSecret)
 QPair<QString, QString> QOAuth1::clientCredentials() const
 {
     Q_D(const QOAuth1);
-    return d->clientCredentials;
+    return qMakePair(d->clientIdentifier, d->clientIdentifierSharedKey);
 }
 
 void QOAuth1::setClientCredentials(const QPair<QString, QString> &clientCredentials)
@@ -303,32 +288,17 @@ void QOAuth1::setClientCredentials(const QString &clientIdentifier,
     setClientSharedSecret(clientSharedSecret);
 }
 
-QString QOAuth1::token() const
-{
-    Q_D(const QOAuth1);
-    return d->tokenCredentials.first;
-}
-
-void QOAuth1::setToken(const QString &token)
-{
-    Q_D(QOAuth1);
-    if (d->tokenCredentials.first != token) {
-        d->tokenCredentials.first = token;
-        Q_EMIT tokenChanged(token);
-    }
-}
-
 QString QOAuth1::tokenSecret() const
 {
     Q_D(const QOAuth1);
-    return d->tokenCredentials.second;
+    return d->clientIdentifierSharedKey;
 }
 
 void QOAuth1::setTokenSecret(const QString &tokenSecret)
 {
     Q_D(QOAuth1);
-    if (d->tokenCredentials.second != tokenSecret) {
-        d->tokenCredentials.second = tokenSecret;
+    if (d->tokenSecret != tokenSecret) {
+        d->tokenSecret = tokenSecret;
         Q_EMIT tokenSecretChanged(tokenSecret);
     }
 }
@@ -336,7 +306,7 @@ void QOAuth1::setTokenSecret(const QString &tokenSecret)
 QPair<QString, QString> QOAuth1::tokenCredentials() const
 {
     Q_D(const QOAuth1);
-    return d->tokenCredentials;
+    return qMakePair(d->token, d->tokenSecret);
 }
 
 void QOAuth1::setTokenCredentials(const QPair<QString, QString> &tokenCredentials)
@@ -483,8 +453,9 @@ QNetworkReply *QOAuth1::requestTemporaryCredentials(QNetworkAccessManager::Opera
 {
     // https://tools.ietf.org/html/rfc5849#section-2.1
     Q_D(QOAuth1);
-    d->tokenCredentials = QPair<QString, QString>();
-    return d->requestToken(operation, url, d->tokenCredentials, parameters);
+    d->token.clear();
+    d->tokenSecret.clear();
+    return d->requestToken(operation, url, qMakePair(d->token, d->tokenSecret), parameters);
 }
 
 QNetworkReply *QOAuth1::requestTokenCredentials(QNetworkAccessManager::Operation operation,
@@ -513,9 +484,9 @@ void QOAuth1::setup(QNetworkRequest *request,
 
     const auto currentDateTime = QDateTime::currentDateTimeUtc();
 
-    oauthParams.insert(Key::oauthConsumerKey, d->clientCredentials.first);
+    oauthParams.insert(Key::oauthConsumerKey, d->clientIdentifier);
     oauthParams.insert(Key::oauthVersion, QStringLiteral("1.0"));
-    oauthParams.insert(Key::oauthToken, d->tokenCredentials.first);
+    oauthParams.insert(Key::oauthToken, d->token);
     oauthParams.insert(Key::oauthSignatureMethod, d->signatureMethodString());
     oauthParams.insert(Key::oauthNonce, QOAuth1::nonce());
     oauthParams.insert(Key::oauthTimestamp, QString::number(currentDateTime.toTime_t()));
@@ -598,7 +569,7 @@ void QOAuth1::grant()
         qWarning("QOAuth1::grant: authorizationGrantUrl is empty");
         return;
     }
-    if (!d->tokenCredentials.first.isEmpty()) {
+    if (!d->token.isEmpty()) {
         qWarning("QOAuth1::grant: Already authenticated");
         return;
     }
@@ -612,11 +583,11 @@ void QOAuth1::grant()
                 // try upgrading token without verifier
                 auto reply = requestTokenCredentials(QNetworkAccessManager::PostOperation,
                                                      d->tokenCredentialsUrl,
-                                                     d->tokenCredentials);
+                                                     qMakePair(d->token, d->tokenSecret));
                 connect(reply, &QNetworkReply::finished, reply, &QNetworkReply::deleteLater);
             } else {
                 QVariantMap parameters;
-                parameters.insert(Key::oauthToken, d->tokenCredentials.first);
+                parameters.insert(Key::oauthToken, d->token);
                 if (d->modifyParametersFunction)
                     d->modifyParametersFunction(Stage::RequestingAuthorization, &parameters);
 
@@ -660,7 +631,7 @@ void QOAuth1::continueGrantWithVerifier(const QString &verifier)
     parameters.insert(Key::oauthVerifier, verifier);
     auto reply = requestTokenCredentials(QNetworkAccessManager::PostOperation,
                                          d->tokenCredentialsUrl,
-                                         d->tokenCredentials,
+                                         qMakePair(d->token, d->tokenSecret),
                                          parameters);
     connect(reply, &QNetworkReply::finished, reply, &QNetworkReply::deleteLater);
 }

src/oauth/qabstractoauth.cpp
@@ -258,6 +258,13 @@ QAbstractOAuthPrivate::QAbstractOAuthPrivate(QNetworkAccessManager *manager) :
 
 QAbstractOAuthPrivate::QAbstractOAuthPrivate(const QUrl &authorizationUrl,
                                              QNetworkAccessManager *manager) :
+    QAbstractOAuthPrivate(authorizationUrl, QString(), manager)
+{}
+
+QAbstractOAuthPrivate::QAbstractOAuthPrivate(const QUrl &authorizationUrl,
+                                             const QString &clientIdentifier,
+                                             QNetworkAccessManager *manager) :
+    clientIdentifier(clientIdentifier),
     authorizationUrl(authorizationUrl),
     defaultReplyHandler(new QOAuthOobReplyHandler),
     networkAccessManagerPointer(manager)
@@ -350,6 +357,57 @@ QAbstractOAuth::QAbstractOAuth(QAbstractOAuthPrivate &dd, QObject *parent)
 */
 QAbstractOAuth::~QAbstractOAuth()
 {}
+
+/*!
+    Returns the current client identifier used in the authentication
+    process.
+
+    \sa setClientIdentifier()
+*/
+QString QAbstractOAuth::clientIdentifier() const
+{
+    Q_D(const QAbstractOAuth);
+    return d->clientIdentifier;
+}
+
+/*!
+    Sets the current client identifier to \a clientIdentifier.
+
+    \sa clientIdentifier()
+*/
+void QAbstractOAuth::setClientIdentifier(const QString &clientIdentifier)
+{
+    Q_D(QAbstractOAuth);
+    if (d->clientIdentifier != clientIdentifier) {
+        d->clientIdentifier = clientIdentifier;
+        Q_EMIT clientIdentifierChanged(clientIdentifier);
+    }
+}
+
+/*!
+    Returns the token used to sign the authenticated requests.
+
+    \sa setToken()
+*/
+QString QAbstractOAuth::token() const
+{
+    Q_D(const QAbstractOAuth);
+    return d->token;
+}
+
+/*!
+    Sets the token used to sign authenticated requests to \a token.
+
+    \sa token()
+*/
+void QAbstractOAuth::setToken(const QString &token)
+{
+    Q_D(QAbstractOAuth);
+    if (d->token != token) {
+        d->token = token;
+        Q_EMIT tokenChanged(token);
+    }
+}
 
 /*!
     Returns the current network access manager used to send the

src/oauth/qabstractoauth.h
@@ -58,6 +58,11 @@ class Q_OAUTH_EXPORT  QAbstractOAuth : public QObject
     Q_ENUMS(Status)
     Q_ENUMS(Stage)
     Q_ENUMS(Error)
+    Q_PROPERTY(QString clientIdentifier
+               READ clientIdentifier
+               WRITE setClientIdentifier
+               NOTIFY clientIdentifierChanged)
+    Q_PROPERTY(QString token READ token WRITE setToken NOTIFY tokenChanged)
     Q_PROPERTY(Status status  READ status NOTIFY statusChanged)
     Q_PROPERTY(QVariantMap extraTokens READ extraTokens NOTIFY extraTokensChanged)
     Q_PROPERTY(QUrl authorizationUrl
@@ -102,11 +107,11 @@ public:
 
     virtual ~QAbstractOAuth();
 
-    virtual QString clientIdentifier() const = 0;
-    virtual void setClientIdentifier(const QString &clientIdentifier) = 0;
+    QString clientIdentifier() const;
+    void setClientIdentifier(const QString &clientIdentifier);
 
-    virtual QString token() const = 0;
-    virtual void setToken(const QString &token) = 0;
+    QString token() const;
+    void setToken(const QString &token);
 
     QNetworkAccessManager *networkAccessManager() const;
     void setNetworkAccessManager(QNetworkAccessManager *networkAccessManager);

src/oauth/qabstractoauth2.cpp
@@ -137,7 +137,8 @@ const QString Key::tokenType =          QStringLiteral("token_type");
 QAbstractOAuth2Private::QAbstractOAuth2Private(const QPair<QString, QString> &clientCredentials,
                                                const QUrl &authorizationUrl,
                                                QNetworkAccessManager *manager) :
-    QAbstractOAuthPrivate(authorizationUrl, manager), clientCredentials(clientCredentials)
+    QAbstractOAuthPrivate(authorizationUrl, clientCredentials.first, manager),
+    clientIdentifierSharedKey(clientCredentials.second)
 {}
 
 QAbstractOAuth2Private::QAbstractOAuth2Private(QNetworkAccessManager *manager) :
@@ -190,6 +191,15 @@ QAbstractOAuth2::QAbstractOAuth2(QNetworkAccessManager *manager, QObject *parent
 QAbstractOAuth2::QAbstractOAuth2(QAbstractOAuth2Private &dd, QObject *parent) :
     QAbstractOAuth(dd, parent)
 {}
+
+void QAbstractOAuth2::setResponseType(const QString &responseType)
+{
+    Q_D(QAbstractOAuth2);
+    if (d->responseType != responseType) {
+        d->responseType = responseType;
+        Q_EMIT responseTypeChanged(responseType);
+    }
+}
 
 /*!
     Destroys the QAbstractOAuth2 instance.
@@ -330,48 +340,28 @@ void QAbstractOAuth2::setUserAgent(const QString &userAgent)
     }
 }
 
-QString QAbstractOAuth2::clientIdentifier() const
+/*!
+    Returns the \l {https://tools.ietf.org/html/rfc6749#section-3.1.1}
+    {response_type} used.
+*/
+QString QAbstractOAuth2::responseType() const
 {
     Q_D(const QAbstractOAuth2);
-    return d->clientCredentials.first;
-}
-
-void QAbstractOAuth2::setClientIdentifier(const QString &clientIdentifier)
-{
-    Q_D(QAbstractOAuth2);
-    if (d->clientCredentials.first != clientIdentifier) {
-        d->clientCredentials.first = clientIdentifier;
-        Q_EMIT clientIdentifierChanged(clientIdentifier);
-    }
+    return d->responseType;
 }
 
 QString QAbstractOAuth2::clientIdentifierSharedKey() const
 {
     Q_D(const QAbstractOAuth2);
-    return d->clientCredentials.second;
+    return d->clientIdentifierSharedKey;
 }
 
 void QAbstractOAuth2::setClientIdentifierSharedKey(const QString &clientIdentifierSharedKey)
 {
     Q_D(QAbstractOAuth2);
-    if (d->clientCredentials.second != clientIdentifierSharedKey) {
-        d->clientCredentials.second = clientIdentifierSharedKey;
+    if (d->clientIdentifierSharedKey != clientIdentifierSharedKey) {
+        d->clientIdentifierSharedKey = clientIdentifierSharedKey;
         Q_EMIT clientIdentifierSharedKeyChanged(clientIdentifierSharedKey);
-    }
-}
-
-QString QAbstractOAuth2::token() const
-{
-    Q_D(const QAbstractOAuth2);
-    return d->token;
-}
-
-void QAbstractOAuth2::setToken(const QString &token)
-{
-    Q_D(QAbstractOAuth2);
-    if (d->token != token) {
-        d->token = token;
-        Q_EMIT tokenChanged(token);
     }
 }
 

src/oauth/qabstractoauth2.h
@@ -45,10 +45,6 @@ class Q_OAUTH_EXPORT QAbstractOAuth2 : public QAbstractOAuth
     Q_OBJECT
     Q_PROPERTY(QString scope READ scope WRITE setScope NOTIFY scopeChanged)
     Q_PROPERTY(QString userAgent READ userAgent WRITE setUserAgent NOTIFY userAgentChanged)
-    Q_PROPERTY(QString clientIdentifier
-               READ clientIdentifier
-               WRITE setClientIdentifier
-               NOTIFY clientIdentifierChanged)
     Q_PROPERTY(QString clientIdentifierSharedKey
                READ clientIdentifierSharedKey
                WRITE setClientIdentifierSharedKey
@@ -80,15 +76,10 @@ public:
     QString userAgent() const;
     void setUserAgent(const QString &userAgent);
 
-    virtual QString responseType() const = 0;
+    QString responseType() const;
 
-    QString clientIdentifier() const override;
-    void setClientIdentifier(const QString &clientIdentifier) override;
     QString clientIdentifierSharedKey() const;
     void setClientIdentifierSharedKey(const QString &clientIdentifierSharedKey);
-
-    QString token() const override;
-    void setToken(const QString &token) override;
 
     QString state() const;
     void setState(const QString &state);
@@ -98,7 +89,7 @@ public:
 Q_SIGNALS:
     void scopeChanged(const QString &scope);
     void userAgentChanged(const QString &userAgent);
-    void clientIdentifierChanged(const QString &clientIdentifier);
+    void responseTypeChanged(const QString &responseType);
     void clientIdentifierSharedKeyChanged(const QString &clientIdentifierSharedKey);
     void stateChanged(const QString &state);
     void expirationAtChanged(const QDateTime &expiration);
@@ -108,6 +99,8 @@ Q_SIGNALS:
 
 protected:
     explicit QAbstractOAuth2(QAbstractOAuth2Private &, QObject *parent = nullptr);
+
+    void setResponseType(const QString &responseType);
 
 private:
     Q_DECLARE_PRIVATE(QAbstractOAuth2)

src/oauth/qabstractoauth2_p.h
@@ -71,11 +71,11 @@ public:
     static QString generateRandomState();
     QNetworkRequest createRequest(QUrl url, const QVariantMap *parameters = nullptr);
 
-    QPair<QString, QString> clientCredentials;
-    QString token;
+    QString clientIdentifierSharedKey;
     QString scope;
     QString state = generateRandomState();
     QString userAgent = QStringLiteral("QtOAuth/1.0 (+https://www.qt.io)");
+    QString responseType;
     const QString bearerFormat = QStringLiteral("Bearer %1"); // Case sensitive
     QDateTime expiresAt;
     QString refreshToken;

src/oauth/qabstractoauth_p.h
@@ -68,11 +68,17 @@ class Q_AUTOTEST_EXPORT QAbstractOAuthPrivate : public QObjectPrivate
 public:
     QAbstractOAuthPrivate(QNetworkAccessManager *manager);
     QAbstractOAuthPrivate(const QUrl &authorizationUrl, QNetworkAccessManager *manager);
+    QAbstractOAuthPrivate(const QUrl &authorizationUrl,
+                          const QString &clientIdentifier,
+                          QNetworkAccessManager *manager);
     ~QAbstractOAuthPrivate();
 
     QNetworkAccessManager *networkAccessManager();
     void setStatus(QAbstractOAuth::Status status);
     static QByteArray generateRandomString(quint8 length);
+
+    QString clientIdentifier;
+    QString token;
 
     // Resource Owner Authorization: https://tools.ietf.org/html/rfc5849#section-2.2
     QUrl authorizationUrl;

src/oauth/qoauth1.h
@@ -64,9 +64,6 @@ public:
 
     ~QOAuth1();
 
-    // Client credentials
-    QString clientIdentifier() const override;
-    void setClientIdentifier(const QString &clientIdentifier) override;
     QString clientSharedSecret() const;
     void setClientSharedSecret(const QString &clientSharedSecret);
     QPair<QString, QString> clientCredentials() const;
@@ -74,8 +71,6 @@ public:
     void setClientCredentials(const QString &clientIdentifier, const QString &clientSharedSecret);
 
     // Token credentials: https://tools.ietf.org/html/rfc5849#section-2.3
-    QString token() const override;
-    void setToken(const QString &token) override;
     QString tokenSecret() const;
     void setTokenSecret(const QString &tokenSecret);
     QPair<QString, QString> tokenCredentials() const;

commit f6aedc74c4a95440e649b625b6272f29e7f5fa71

Fix QOAuth1::tokenSecret() return value
Commit c071b588ce2b4d474845d553aa83120747dc46da broke QOAuth1::tokenSecret(),
in fact, it were returning the clientIdentifierSharedKey instead.

[ChangeLog][QOAuth1] Fix tokenSecret() return value.

Task-number: QTBUG-65422
Change-Id: I72d6783220e7a030eac65e5c22e4421a1d6b106e
Reviewed-by: Mårten Nordheim <marten.nordheim@qt.io>
Reviewed-by: Jesus Fernandez <Jesus.Fernandez@qt.io>

src/oauth/qoauth1.cpp
@@ -391,7 +391,7 @@ void QOAuth1::setClientCredentials(const QString &clientIdentifier,
 QString QOAuth1::tokenSecret() const
 {
     Q_D(const QOAuth1);
-    return d->clientIdentifierSharedKey;
+    return d->tokenSecret;
 }
 /*!
     Sets \a tokenSecret as the current token secret used to sign

tests/auto/oauth1/tst_oauth1.cpp
@@ -145,6 +145,7 @@ public Q_SLOTS:
 private Q_SLOTS:
     void clientIdentifierSignal();
     void clientSharedSecretSignal();
+    void tokenSignal();
     void tokenSecretSignal();
     void temporaryCredentialsUrlSignal();
     void temporaryTokenCredentialsUrlSignal();
@@ -252,7 +253,7 @@ void tst_OAuth1::clientSharedSecretSignal()
     PropertyTester::run(&QOAuth1::clientSharedSecretChanged, setters);
 }
 
-void tst_OAuth1::tokenSecretSignal()
+void tst_OAuth1::tokenSignal()
 {
     using PropertyTester = PropertyTester<QString>;
     PropertyTester::SetterFunctions setters {
@@ -266,6 +267,22 @@ void tst_OAuth1::tokenSecretSignal()
         }
     };
     PropertyTester::run(&QOAuth1::tokenChanged, setters);
+}
+
+void tst_OAuth1::tokenSecretSignal()
+{
+    using PropertyTester = PropertyTester<QString>;
+    PropertyTester::SetterFunctions setters {
+        [](QString *expectedValue, QOAuth1 *object) {
+            *expectedValue = "setTokenSecret";
+            object->setTokenSecret(*expectedValue);
+        },
+        [](QString *expectedValue, QOAuth1 *object) {
+            *expectedValue = "setTokenCredentials";
+            object->setTokenCredentials(qMakePair(QString(), *expectedValue));
+        }
+    };
+    PropertyTester::run(&QOAuth1::tokenSecretChanged, setters);
 }
 
 void tst_OAuth1::temporaryCredentialsUrlSignal()