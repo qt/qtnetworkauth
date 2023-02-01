c6c56022f5897cba5d1d285953e172cdcacd9819
Friedemann Kleint <Friedemann.Kleint@qt.io>
2023-02-01 13:47:40 +0100

    Example: Add missing include guards
    Task-number: QTBUG-109394
    Change-Id: Idc20c0312409add1f28d76d7670a690b5af99d7d
    Reviewed-by: Ivan Solovev <ivan.solovev@qt.io>
    (cherry picked from commit 485c94c5127d2899d0d36502d7f4a248e8ec48fd)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- examples/oauth/twittertimeline/twittertimelinemodel.h
+++ examples/oauth/twittertimeline/twittertimelinemodel.h
@@ -1,6 +1,9 @@
 // Copyright (C) 2017 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
 
+#ifndef TWITTERTIMELINEMODEL_H
+#define TWITTERTIMELINEMODEL_H
+
 #include "twitter.h"
 
 #include <QtCore>
@@ -42,3 +45,5 @@ private:
     QList<Tweet> tweets;
     Twitter twitter;
 };
+
+#endif // TWITTERTIMELINEMODEL_H
