b7905d91983b790cc4eefcad3f759bc95f63bc9c
Thiago Macieira <thiago.macieira@intel.com>
2024-05-08 12:47:28 -0700

    QAbstractOAuth: use QByteArray::toBase64() to encode random to text
    The alphabet we had was 62 characters long, which made for clumsy
    division/modulus operations. If we extend to 64, the operations are
    simpler. And if we're going to do that anyway, we may as well use
    QByteArray to encode pure random data.
    
    That's what I use to generate random strings, actually:
     $ dd if=/dev/random bs=3 count=4 status=none | basenc --base64url
     IjlwaN9HD_7HeNX0
    
    Change-Id: Ie30a3caf09ef4176bb36fffd17cd9b2c7a5cf58c
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>
    (cherry picked from commit 90d5571d02887a8f61ec767699bb05c76c437153)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 5ac0a08a08821be91f2c3a2d143b17483b164daf)
    (cherry picked from commit d20f62567e3ef4acf30443876f86ca6c7ffe8273)

--- tests/auto/abstractoauth/tst_abstractoauth.cpp
+++ tests/auto/abstractoauth/tst_abstractoauth.cpp
@@ -42,6 +42,8 @@ private:
 
 private Q_SLOTS:
     void authorizationUrlSignal();
+    void generateRandomString_data();
+    void generateRandomString();
 };
 
 void tst_AbstractOAuth::authorizationUrlSignal()
@@ -57,6 +59,53 @@ void tst_AbstractOAuth::authorizationUrlSignal()
     });
     obj.setAuthorizationUrl(expectedValue);
     QVERIFY(emitted);
+}
+
+void tst_AbstractOAuth::generateRandomString_data()
+{
+    QTest::addColumn<int>("length");
+    for (int i = 0; i <= 255; ++i)
+        QTest::addRow("%d", i) << i;
+}
+
+// copied from https://xkcd.com/221/
+int getRandomNumber()
+{
+    return 4;   // chosen by fair dice roll.
+                // guaranteed to be random.
+}
+
+void tst_AbstractOAuth::generateRandomString()
+{
+    struct Unprotected : public QAbstractOAuth {
+        using QAbstractOAuth::generateRandomString;
+    };
+
+    QFETCH(int, length);
+    QByteArray random1 = Unprotected::generateRandomString(length);
+    QCOMPARE(random1.size(), length);
+
+    // Check that it is truly random by repeating and checking that it is
+    // different. We don't try it for 1 and 2 characters because the chance of
+    // random coincidence is too high: 1 in 2^(6*n), so 1 in 64 and 1 in 4096
+    // respectively. For 3 characters, that decreases to 1 in 262,144.
+    if (length <= 2)
+        return;
+
+    QByteArray random2 = Unprotected::generateRandomString(length);
+    QCOMPARE_NE(random2, random1);
+
+    // Generate a Base64 string using getRandomNumber() random bytes. Base64
+    // encodes 6 bits per byte, so a 255-character string has 1530 bits of
+    // data.
+    char buf[192] = {};
+    int rawlen = (length * 6 + 7) / 8;
+    for (int i = 0; i < rawlen; ++i)
+        buf[i] = getRandomNumber();
+    QByteArray random3 = QByteArray(buf, rawlen).toBase64(QByteArray::Base64UrlEncoding);
+    Q_ASSERT(random3.size() >= length);
+    random3.truncate(length);
+    QCOMPARE_NE(random3, random1);
 }
 
 QTEST_MAIN(tst_AbstractOAuth)
