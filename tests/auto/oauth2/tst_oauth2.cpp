c15b888c1438aaab66a874217a34ae92b2383f91
Ivan Solovev <ivan.solovev@qt.io>
2025-01-13 15:26:38 +0100

    Fix TLS authentication tests on Windows
    On both my Windows machines the tests always reports all three expected
    errors, but the CI seems to be ok with the old version of the test.
    This might be related to schannel vs openssl backend implementation, so
    use QCOMPARE_GE() to make both of them happy. Also add a QCOMPARE_LE()
    check to make sure that we do not get too many errors.
    We anyway verify that we get only the expected errors later on, so this
    shouldn't break any tests.
    
    Conflict resolution for 6.8: remove tst_oauth2deviceflow.cpp, as it was
    only added in 6.9.
    
    Amends db2f209b2d299def846503e632dc9999e3007cba.
    
    Change-Id: I16c5d442b9ddc62dae3e3a22d0b7327661521d80
    Reviewed-by: Juha Vuolle <juha.vuolle@qt.io>
    Reviewed-by: Mårten Nordheim <marten.nordheim@qt.io>
    (cherry picked from commit 0506848dbf61696499f2b94364ffd14993c15041)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit 6da1113061448aec4432b86bc511e7cb97c38ca8)
    (cherry picked from commit fe9f43ddeefc7fc3014b7388e7a57e60a763a2ff)

--- tests/auto/oauth2/tst_oauth2.cpp
+++ tests/auto/oauth2/tst_oauth2.cpp
@@ -424,7 +424,10 @@ void tst_OAuth2::tlsAuthentication()
     });
     connect(&nam, &QNetworkAccessManager::sslErrors,
         [&expectedErrors](QNetworkReply *r, const QList<QSslError> &errors) {
-            QCOMPARE(errors.size(), 2);
+            // On some Windows machines the test might report all
+            // three expected errors.
+            QCOMPARE_GE(errors.size(), 2);
+            QCOMPARE_LE(errors.size(), 3);
             for (const auto &err : errors)
                 QVERIFY(expectedErrors.contains(err.error()));
             r->ignoreSslErrors();
