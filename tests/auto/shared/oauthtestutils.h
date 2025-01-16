a4d108220ade0dd3f600b2edf96e2f1fd165e7f6
Juha Vuolle <juha.vuolle@qt.io>
2025-01-16 17:22:01 +0200

    Make oauthtestutils not a header-only
    That way it's cleaner as an API (likely to grow over time).
    
    'useTemporaryKeychain' is kept as a header implementation
    due to the 'auto' return value (compiler needs to see the
    definition at compile time to deduce the type).
    
    Change-Id: I3b32d098d0bc1588d3b9c5a1fc69e91c243e3078
    Reviewed-by: Ivan Solovev <ivan.solovev@qt.io>
    (cherry picked from commit 9b98e40be268175506792d171885db118a3a43b9)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- tests/auto/shared/oauthtestutils.h
+++ tests/auto/shared/oauthtestutils.h
@@ -4,20 +4,18 @@
 #ifndef OAUTHTESTUTILS_H
 #define OAUTHTESTUTILS_H
 
+#include <QtNetworkAuth/qoauthglobal.h>
+
 #ifndef QT_NO_SSL
 #include <QtNetwork/qsslconfiguration.h>
-#include <QtNetwork/qsslkey.h>
 #endif
 
-#ifndef QT_NO_SSL
-#include <QtCore/qfile.h>
-#endif
-#include <QtCore/qdatetime.h>
+#include <QtCore/qcontainerfwd.h>
+#include <QtCore/qscopeguard.h>
+#include <QtCore/qstring.h>
 #include <QtCore/qtenvironmentvariables.h>
-#include <QtCore/qjsondocument.h>
-#include <QtCore/qmessageauthenticationcode.h>
 
-[[nodiscard]] static auto useTemporaryKeychain()
+[[nodiscard]] inline auto useTemporaryKeychain()
 {
 #ifndef QT_NO_SSL
     // Set the same environment value as CI uses, so that it's possible
@@ -38,64 +36,11 @@
 #endif // QT_NO_SSL
 }
 
-static QString createSignedJWT(const QVariantMap &header = {}, const QVariantMap &payload = {})
-{
-    auto base64Encode = [](const QByteArray &input) {
-        return input.toBase64(QByteArray::Base64UrlEncoding | QByteArray::OmitTrailingEquals);
-    };
-    // Repeating values which can be overridden and augmented by the supplied 'header' and 'payload'
-    QVariantMap mergedHeader = {{"alg", "HS256"},
-                                {"typ", "JWT"}};
-    QVariantMap mergedPayload = {{"iss", "https://tst_oauth2.example.com"},
-                                 {"sub", "tst_oauth2"},
-                                 {"aud", "tst_oauth2_client_id"},
-                                 {"exp", QDateTime::currentSecsSinceEpoch() + 300}, // valid 5 mins
-                                 {"iat", QDateTime::currentSecsSinceEpoch()}, // issued now
-                                 {"name", "No Body"},
-                                 {"email", "no.body@example.com"}};
-    mergedHeader.insert(header);
-    mergedPayload.insert(payload);
-
-    // Signed JWT within OIDC context is: header.payload.signature (separated with dots)
-    auto header64 =
-        base64Encode(QJsonDocument::fromVariant(mergedHeader).toJson(QJsonDocument::Compact));
-    auto payload64 =
-        base64Encode(QJsonDocument::fromVariant(mergedPayload).toJson(QJsonDocument::Compact));
-    auto token = header64 + "." + payload64;
-    auto signature64 =
-        base64Encode(QMessageAuthenticationCode::hash(token, "secret", QCryptographicHash::Sha256));
-    token = token + "." + signature64;
-    return token;
-}
+QString createSignedJWT(const QVariantMap &header = {}, const QVariantMap &payload = {});
 
 #ifndef QT_NO_SSL
-static QSslConfiguration createSslConfiguration(QString keyFileName, QString certificateFileName)
-{
-    QSslConfiguration configuration(QSslConfiguration::defaultConfiguration());
-
-    QFile keyFile(keyFileName);
-    if (keyFile.open(QIODevice::ReadOnly)) {
-        QSslKey key(keyFile.readAll(), QSsl::Rsa, QSsl::Pem, QSsl::PrivateKey);
-        if (!key.isNull()) {
-            configuration.setPrivateKey(key);
-        } else {
-            qCritical() << "Could not parse key: " << keyFileName;
-        }
-    } else {
-        qCritical() << "Could not find key: " << keyFileName;
-    }
-
-    QList<QSslCertificate> localCert = QSslCertificate::fromPath(certificateFileName);
-    if (!localCert.isEmpty() && !localCert.first().isNull()) {
-        configuration.setLocalCertificate(localCert.first());
-    } else {
-        qCritical() << "Could not find certificate: " << certificateFileName;
-    }
-
-    configuration.setPeerVerifyMode(QSslSocket::VerifyPeer);
-
-    return configuration;
-}
+QSslConfiguration createSslConfiguration(const QString &keyFileName,
+                                         const QString &certificateFileName);
 #endif // QT_NO_SSL
 
 #endif // OAUTHTESTUTILS_H
