3d8136695f72887b74f1d560930b74f0bc5bdb07
Juha Vuolle <juha.vuolle@qt.io>
2025-04-03 15:09:19 +0300

    Mark security critical files
    Most cpp files in this module are security critical, as
    this module implements network interfacing authorization
    protocols.
    
    Headers in this module are not marked as they are
    trivial.
    
    Fixes: QTBUG-135564
    Pick-to: 6.8
    Change-Id: I97d8079296a98099955d3fe63434279599911a5e
    Reviewed-by: Ivan Solovev <ivan.solovev@qt.io>
    (cherry picked from commit 42d499e645ad9edd7941805b321da783e4a82cf3)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/oauth/qoauth2deviceauthorizationflow.cpp
+++ src/oauth/qoauth2deviceauthorizationflow.cpp
@@ -1,5 +1,6 @@
 // Copyright (C) 2024 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only
+// Qt-Security score:critical reason:authorization-protocol
 
 #include <private/qoauth2deviceauthorizationflow_p.h>
 #include <QtNetworkAuth/qoauth2deviceauthorizationflow.h>
--- src/oauth/qoauthurischemereplyhandler.cpp
+++ src/oauth/qoauthurischemereplyhandler.cpp
@@ -1,5 +1,6 @@
 // Copyright (C) 2024 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only
+// Qt-Security score:critical reason:authorization-protocol
 
 #include "qabstractoauthreplyhandler_p.h" // for lcReplyHandler()
 #include "qoauthoobreplyhandler_p.h"
