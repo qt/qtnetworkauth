66ff1debbecb30fc71d459a892955f98fd6722cb
Tarja Sundqvist <tarja.sundqvist@qt.io>
2025-05-16 19:47:09 +0300

    Update commercial SPDX-License-Identifier
    Updated the commercial SPDX-License-Identifier to
    the source files. Examples, tests, documentation,
    or build files are not updated.
    
    Modified licenseRule.json accordingly.
    
    Task-number: QTBUG-136484
    Change-Id: I57ad79f1242d39a5a761517eed86903a14983f45
    Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>

--- src/oauth/qoauth2authorizationcodeflow.h
+++ src/oauth/qoauth2authorizationcodeflow.h
@@ -1,5 +1,5 @@
 // Copyright (C) 2017 The Qt Company Ltd.
-// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial
 
 #ifndef QOAUTH2AUTHORIZATIONCODEFLOW_H
 #define QOAUTH2AUTHORIZATIONCODEFLOW_H
--- src/oauth/qoauthurischemereplyhandler.cpp
+++ src/oauth/qoauthurischemereplyhandler.cpp
@@ -1,5 +1,5 @@
 // Copyright (C) 2024 The Qt Company Ltd.
-// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial
 // Qt-Security score:critical reason:authorization-protocol
 
 #include "qabstractoauthreplyhandler_p.h" // for lcReplyHandler()
