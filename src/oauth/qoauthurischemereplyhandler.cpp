024a0a1899e9181ecf9d291ed760bc2b6f34275e
Tarja Sundqvist <tarja.sundqvist@qt.io>
2025-09-10 12:52:04 +0300

    Revert "Update commercial SPDX-License-Identifier"
    This reverts commit 66ff1debbecb30fc71d459a892955f98fd6722cb.
    
    The revert is needed for the Qt 6.8 LTS opensource releases.
    They cannot contain commercial SPDX license identifiers.
    
    Task-number: QTLTSOSS-264
    Change-Id: I80f34dac14fd43f62835189e5d89b573f45cfeb3
    Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>

--- src/oauth/qoauthurischemereplyhandler.cpp
+++ src/oauth/qoauthurischemereplyhandler.cpp
@@ -1,5 +1,5 @@
 // Copyright (C) 2024 The Qt Company Ltd.
-// SPDX-License-Identifier: LicenseRef-Qt-Commercial
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only
 // Qt-Security score:critical reason:authorization-protocol
 
 #include "qabstractoauthreplyhandler_p.h" // for lcReplyHandler()
